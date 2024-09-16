a418a819bad295f7a7a39b58f621af8212e2465d
Jholman Duvan Rojas Bautista <ing_jholmanrojas@hotmail.com>
2024-09-16 10:42:47 -0500

    modified:   truchas-mutiscua/src/App.css modified:   truchas-mutiscua/src/App.jsx new file:   truchas-mutiscua/src/component/Botones.jsx new file:   truchas-mutiscua/src/component/Cabecera.jsx new file:   truchas-mutiscua/src/component/archivos_css/Botones.css new file:   truchas-mutiscua/src/component/archivos_css/Cabecera.css

---
+++ truchas-mutiscua/src/component/Botones.jsx
@@ -0,0 +1,11 @@
+import './archivos_css/Botones.css'
+
+function Botones(){
+    return(
+        <form>
+            <button type='submit'>REGISTRAR ACTIVIDAD</button>
+            <button type='submit'>HORARIO DE ALIMENTACION</button>
+    </form>
+    )
+}
+export default Botones;
---
+++ truchas-mutiscua/src/component/Cabecera.jsx
@@ -0,0 +1,12 @@
+import './archivos_css/cabecera.css';
+
+function Cabecera() {
+  return (
+    <header className='cabecera'>
+      <h1>GESTOR DE MANEJO DE TRUCHAS</h1>
+      <p>Seleccione la opcion que desea registrar:</p>
+    </header>
+  );
+}
+
+export default Cabecera;
